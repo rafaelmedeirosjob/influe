bff41f6752c3f2341ea576b61fc63da6f1ed93bd
Rafael Medeiros <rafaelmedeirosjob@gmail.com>
2024-04-23 11:22:41 -0300

    feat: add calls to pending proposes

--- data/metrics.ts
+++ data/metrics.ts
@@ -1,14 +1,19 @@
 import { db } from "@/lib/db";
 
-export const getAllProposes = async () => {
+export const getAllProposesPending = async () => {
   try {
-    console.log('Fetching getAllProposes data...');
+    console.log('Fetching getAllProposesPending data...');
     const proposes = await db.proposal.findMany({
+      where: {
+        status: {
+          contains: 'Pending',
+        },
+      }
     });
-    console.log('Fetching getAllProposes data...');
+    console.log('Fetching getAllProposesPending data...');
     return proposes;
   } catch (error) {
     console.error('Database Error:', error);
-    throw new Error('Failed to fetch getAllProposes data.');
+    throw new Error('Failed to fetch getAllProposesPending data.');
   }
 };
--- lib/dashboard.ts
+++ lib/dashboard.ts
@@ -1,22 +1,10 @@
-import { getAllProposes } from "@/data/metrics";
+import { getAllProposesPending } from "@/data/metrics";
 
-export const getCountAllProposes = async () => {
-  
-    const proposes = await getAllProposes();
-  
-    if (proposes) {
-        return [{
-            month: 'September', 
-            revenue: proposes.length
-        }];
-    }
-  
-    return null;
-  };
 
-  export const getOnePropose = async () => {
+
+  export const getCountAllProposes = async () => {
   
-    const proposes = await getAllProposes();
+    const proposes = await getAllProposesPending();
 
     return proposes.length
   };
